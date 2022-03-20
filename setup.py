7375260599efa5e7e53ff0479af157bd3f958b70
David Tonderski <dtonderski@gmail.com>
2022-03-20 16:20:49 -0400

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='data_utils.py',
+    version='0.1',
+    packages=['tests', 'deep_sudoku', 'deep_sudoku.utils'],
+    url='https://github.com/dtonderski/Deep-Sudoku',
+    license='GNU GPLv3',
+    author='davton',
+    author_email='dtonderski@gmail.com',
+    description='Solving Sudokus using a Neural '
+                'Network assisted Monte-Carlo approach.')
