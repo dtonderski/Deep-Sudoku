8206529c8d3ac310d4b4b8bcfcb1280890c18e84
David Tonderski <dtonderski@gmail.com>
2022-03-20 18:29:07 -0400

    Changed underscore to dash, updated setup.py with requirements and changed setup.py name

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='data_utils.py',
+    name='deep-sudoku',
     version='0.1',
-    packages=['tests', 'deep_sudoku', 'deep_sudoku.utils'],
+    packages=['deep-sudoku', 'deep-sudoku.utils'],
     url='https://github.com/dtonderski/Deep-Sudoku',
     license='GNU GPLv3',
     author='davton',
     author_email='dtonderski@gmail.com',
     description='Solving Sudokus using a Neural '
-                'Network assisted Monte-Carlo approach.')
+                'Network assisted Monte-Carlo approach.',
+    install_requires=['numpy', 'py-sudoku']
+)
