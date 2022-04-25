6e8fd43bb6660aacf373857b19c9ad10209313c0
dtonderski <dtonderski@gmail.com>
2022-04-24 22:22:28 -0400

    Changed training, added model definition to package, added natural distribution formed by training the network on a uniform distribution and then using the resulting losses as a distribution after normalizing

---
+++ deepsudoku/__init__.py
@@ -0,0 +1,1 @@
+from deepsudoku import model
---
+++ deepsudoku/model.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+        self.conv0 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(3,3), stride=(1,1), padding=1)
+
+        self.convs = []
+        for i in range(8):
+            self.convs.append(nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=(1,1), padding=1))
+        self.convs = nn.ModuleList(self.convs)
+
+        self.convlast = nn.Conv2d(in_channels=64, out_channels=9, kernel_size=(3,3), stride=(1,1), padding=1)
+
+    def forward(self, x):
+        x = self.conv0(x)
+        x = nn.ReLU()(x)
+        for conv in self.convs:
+            x = conv(x)
+            x = nn.ReLU()(x)
+        x = self.convlast(x)
+        return x
