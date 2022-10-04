762fe3bc27d440540481d5417b15809c410cb1d8
davton <dtonderski@gmail.com>
2022-10-04 15:06:30 -0400

    Added rng_seed parameter to make_random_moves for controlled randomness

--- deepsudoku/utils/sudoku_utils.py
+++ deepsudoku/utils/sudoku_utils.py
@@ -111,7 +111,8 @@ def load_string(string) -> np.ndarray:
 
 
 def make_random_moves(board: np.ndarray, solved: np.ndarray,
-                      n_valid_moves: int, n_invalid_moves: int = 0) \
+                      n_valid_moves: int, n_invalid_moves: int = 0,
+                      rng_seed: int = None) \
         -> np.ndarray:
     """
     Function that takes in an unsolved and a solved board and makes a number of
@@ -120,19 +121,21 @@ def make_random_moves(board: np.ndarray, solved: np.ndarray,
     :param solved: (9,9) solved sudoku board
     :param n_valid_moves: number of valid moves to make
     :param n_invalid_moves: number of invalid, randomly selected moves to make
+    :param rng_seed:
     :return: (9,9) board
     """
 
     new_board = board.copy()
+    rng = np.random.default_rng(rng_seed)
 
     if n_invalid_moves > 0:
         possible_moves = np.argwhere(new_board == 0)
-        invalid_move_indices = np.random.choice(range(len(possible_moves)),
-                                                n_invalid_moves, replace=False)
+        invalid_move_indices = rng.choice(range(len(possible_moves)),
+                                          n_invalid_moves, replace=False)
         invalid_moves = possible_moves[invalid_move_indices]
         correct_values = solved[invalid_moves[:, 0], invalid_moves[:, 1]]
 
-        shifting = np.random.choice(range(1, 9), len(correct_values))
+        shifting = rng.choice(range(1, 9), len(correct_values))
         incorrect_values = np.mod(correct_values + shifting, 9)
         incorrect_values[incorrect_values == 0] = 9
         new_board[invalid_moves[:, 0], invalid_moves[:, 1]] = incorrect_values
@@ -141,8 +144,8 @@ def make_random_moves(board: np.ndarray, solved: np.ndarray,
         possible_moves = np.argwhere(new_board == 0)
         n_valid_moves = max(0, min(n_valid_moves - n_invalid_moves,
                                    len(possible_moves)))
-        valid_move_indices = np.random.choice(range(len(possible_moves)),
-                                              n_valid_moves, replace=False)
+        valid_move_indices = rng.choice(range(len(possible_moves)),
+                                        n_valid_moves, replace=False)
         valid_moves = possible_moves[valid_move_indices]
         new_board[valid_moves[:, 0], valid_moves[:, 1]] = \
             solved[valid_moves[:, 0], valid_moves[:, 1]]
