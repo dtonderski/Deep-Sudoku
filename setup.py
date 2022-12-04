7a62defa9a6bc1668685bd8b52c5df7729f92466
davton <dtonderski@gmail.com>
2022-12-04 18:20:56 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deepsudoku',
-    version='0.6.0',
+    version='0.7.0',
 
     packages=find_packages(),
     url='https://github.com/dtonderski/DeepSudoku',
