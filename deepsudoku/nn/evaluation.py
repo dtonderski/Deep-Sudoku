fe59bbb72071e15f7e3a84fc501256bf9fad3467
davton <dtonderski@gmail.com>
2022-12-04 22:52:37 -0500

    Added categorical accuracy to nn.evaluation

---
+++ deepsudoku/nn/evaluation.py
@@ -0,0 +1,9 @@
+import torch
+
+
+def categorical_accuracy(x: torch.Tensor, y: torch.Tensor,
+                         y_pred: torch.Tensor) -> torch.Tensor:
+    mask = y[1][:, :, None, None] * (x == 0)
+    return torch.logical_and(
+        torch.eq(y_pred[0].argmax(dim=1), y[0]), mask[:, 0]
+    ).sum() / mask.sum()
